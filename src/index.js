d1f844b81b561819472f20766ae097e506af643c
Ideal-Poster <malcolmgourdine@protonmail.com>
2021-02-23 02:32:42 -0500

    listen to login/register

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
 
 import 'semantic-ui-css/semantic.min.css'
+import firebase from './firebase';
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
-    </Switch>
-  </Router>
-)
+const Root = (props) => {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(props);
+        props.history.push('/')
+      }
+    })
+  }, [])
+  
+  return(
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+      </Switch>
+  )
+}
+
+const RootWithAuth = withRouter(Root)
+
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Router>
+      <RootWithAuth />
+    </Router>
   </React.StrictMode>
   ,
   document.getElementById("root")
